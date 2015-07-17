97185698a629f49f9a264e49e93b4abd3120c376
Tom McFarlin <tmcfarlin@usgbc.org>
2015-07-17 09:11:27 -0400

    organize building CRUD into single file

--- versionUtil.py
+++
@@ -1,8 +0,0 @@
-import platform
-
-version_numbers = platform.python_version().split('.')
-
-if (version_numbers[0] == '2'):
-    print "version 2"
-elif (version_numbers[0] == '3'):
-    print("version 3")
